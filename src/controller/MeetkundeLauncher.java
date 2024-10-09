213a8a4a632bd7229894d57ccf6dfaa152cf2091
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2024-10-09 13:35:54 +0200

    Hoofdstuk 9.1

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -2,6 +2,8 @@ package controller;
 
 import model.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -14,26 +16,24 @@ import java.util.Scanner;
 public class MeetkundeLauncher {
 
     public static void main(String[] args) {
-        Scanner toetsenbord = new Scanner(System.in);
-        Cirkel cirkel = null;
 
-        while (cirkel == null) {
-            System.out.print("Geef de straal van de cirkel: ");
+        Oppervlak oppervlak = new Oppervlak(20, 30);
+        try (Scanner stralenScanner = new Scanner(new File("resources/stralen.txt"))) {
 
-            try {
-                double straal = toetsenbord.nextDouble();
-                cirkel = new Cirkel(straal);
-            } catch (IllegalArgumentException illegalArgumentException)  {
-                System.err.println(illegalArgumentException.getMessage());
-            } catch (InputMismatchException inputMismatchException) {
-                toetsenbord.nextLine();
-                System.err.println("Hoe dom kun je zijn? Wat kun je wel? Wat is dit!?!?!?! ");
-            } finally {
-                System.out.println("Of het nu lukt of niet je krijgt dit te zien");
+            while (stralenScanner.hasNextDouble()) {
+                double straal = stralenScanner.nextDouble();
+
+                try {
+                    oppervlak.voegFiguurToe(new Cirkel(straal));
+                } catch (IllegalArgumentException illegalArgumentException) {
+                    System.out.println(illegalArgumentException.getMessage());
+                    System.out.println("De betrokken straal wordt overgeslagen.");
+                }
             }
-        }
 
-        System.out.println(cirkel);
+        } catch (FileNotFoundException e) {
+            System.out.println("Het opgegeven bestand is niet beschikbaar.");
+        }
     }
 
     public static void toonInformatie(Figuur figuur) {
